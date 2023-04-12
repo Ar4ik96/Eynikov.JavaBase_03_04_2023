34a9364d60b7f484b0391c416c75a68e33936158
Eynikov Artur <redmi1245ok@gmail.com>
2023-04-12 19:23:18 +0300

    added new Sout

--- src/main/java/ua/hillel/eynicov/lesson3/Main.java
+++ src/main/java/ua/hillel/eynicov/lesson3/Main.java
@@ -3,5 +3,6 @@ package ua.hillel.eynicov.lesson3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("My name is Artur Eynikov, I will soon become a Java developer");
     }
 }
