4899c5bc2cd840f4be44fa73c7fa28beedb7005e
Eynikov Artur <redmi1245ok@gmail.com>
2023-04-28 15:55:07 +0300

    :)

--- src/main/java/ua/hillel/eynicov/Lesson6/HomeWorkShuttle.java
+++ src/main/java/ua/hillel/eynicov/Lesson6/HomeWorkShuttle.java
@@ -3,12 +3,15 @@ package ua.hillel.eynicov.Lesson6;
 public class HomeWorkShuttle {
     public static void main(String[] args) {
         System.out.println("Lucky shuttle numbers:");
+        int counter = 0;
 
-        for (int i = 1; i <= 100; i++) {
-            if (i % 10 == 4 || i % 10 == 9 || i / 10 == 4 || i / 10 == 9) {
-                continue;
+        for (int i = 1; counter < 100; i++) {
+          if ((String.valueOf(i).contains("4") || String.valueOf(i).contains("9"))){
+              continue;
             }
-            System.out.println(i);
+            System.out.println("Shuttle number: " + i);
+          counter++;
         }
+        System.out.println("Counter shuttle = " + counter);
     }
 }
